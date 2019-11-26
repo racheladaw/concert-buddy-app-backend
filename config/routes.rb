6fdc1707f5ded2d0a9b103489a571e7aeee62353
Rachel <williamsrachelada@gmail.com>
2019-11-25 16:59:02 -0800

    Added comments for namespacing api routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # namespace routes to look like localhost:3000.com/api/v1/resource
+  # namespace :api do
+  #   namespace :v1 do
+  #     resources :users
+  #   end
+  # end
 end
