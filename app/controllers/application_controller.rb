5d958f21dcdde01abc7b954aa4f6074b1c14c49e
Rachel <williamsrachelada@gmail.com>
2019-11-27 15:28:32 -0800

    Added helper methods to mock a logged in user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::API
+  helpers do
+    def current_user
+      #mocked version of being logged in
+      User.first
+    end
+
+    def logged_in?
+      !!current_user
+    end
+  end
 end
