69cae5c9accda1f4839f3c11e468b63d8b3d0eff
Rachel <williamsrachelada@gmail.com>
2019-11-27 14:27:40 -0800

    Fixed relationship in user model and added initial seeds to db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+rachel = User.create(name: "Rachel", username: "rachelw", password: "password")
+
+concert1 = Concert.create(date: "11-28-2019", price_range: "50-100", upcoming: true)
+
+rachel.saved_concerts.create(concert_id: concert1)
