39dde0e7cc655c3c7894ef917895a91960e92a6a
Rachel <williamsrachelada@gmail.com>
2019-11-27 14:35:14 -0800

    Created locations table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_27_221332) do
+ActiveRecord::Schema.define(version: 2019_11_27_223250) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,14 @@ ActiveRecord::Schema.define(version: 2019_11_27_221332) do
     t.date "date"
     t.string "price_range"
     t.boolean "upcoming"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "locations", force: :cascade do |t|
+    t.string "city"
+    t.string "state"
+    t.string "country"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
