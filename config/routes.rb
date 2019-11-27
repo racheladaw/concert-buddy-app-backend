e27fb9a38abefa711933cea7b438c2659147edb7
Rachel <williamsrachelada@gmail.com>
2019-11-27 14:00:19 -0800

    Added users resource to routes and namespaced into api v1

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   # namespace routes to look like localhost:3000.com/api/v1/resource
-  # namespace :api do
-  #   namespace :v1 do
-  #     resources :users
-  #   end
-  # end
+  namespace :api do
+    namespace :v1 do
+      resources :users
+    end
+  end
 end
