54d59c843557b410d23b06d546f8af9c94ee34d4
Rachel <williamsrachelada@gmail.com>
2019-11-27 15:45:02 -0800

    Removed helper block wrapping methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,10 @@
 class ApplicationController < ActionController::API
-  helpers do
-    def current_user
-      #mocked version of being logged in
-      User.first
-    end
+  def current_user
+    #mocked version of being logged in
+    User.first
+  end
 
-    def logged_in?
-      !!current_user
-    end
+  def logged_in?
+    !!current_user
   end
 end
