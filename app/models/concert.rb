c7f5f16531882c8d4cc8f6808d4e5bc130922c5a
Rachel <williamsrachelada@gmail.com>
2019-11-27 14:18:36 -0800

    Added relationships user, concert, and saved concert models

--- app/models/concert.rb
+++ app/models/concert.rb
@@ -1,2 +1,4 @@
 class Concert < ApplicationRecord
+  has_many :saved_concerts
+  has_many :users, through: :saved_concerts
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ApplicationRecord
   has_secure_password
+  has_many :saved_concerts
+  has_many :concerts, through :saved_concerts
 end
