184fbe9e69d0c6b1d20b76aefd39fca6f6479d72
Rachel <williamsrachelada@gmail.com>
2019-12-05 18:11:48 -0800

    Created getCurrentUser action to find current user from session and render user json

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -3,10 +3,21 @@ class Api::V1::SessionsController < ApplicationController
     @user = User.find_by(username: params[:session][:username])
 
     if @user && @user.authenticate(params[:session][:password])
+      session[:user_id] = @user.id
       render json: @user
     else
       render json: {
         error: "Invalid Credentials"
+      }
+    end
+  end
+
+  def getCurrentUser
+    if logged_in?
+      render json: current_user
+    else
+      render json: {
+        error: "You are not logged in"
       }
     end
   end
