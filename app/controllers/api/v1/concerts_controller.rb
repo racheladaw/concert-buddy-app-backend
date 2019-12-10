351559fa5e1f18291414b6416101491ca95098cb
Rachel <williamsrachelada@gmail.com>
2019-12-10 13:13:07 -0800

    Added upcoming concerts action to grab upcoming concerts based on location of user

--- app/controllers/api/v1/concerts_controller.rb
+++ app/controllers/api/v1/concerts_controller.rb
@@ -1,2 +1,11 @@
-class Api::V1::ConcertsController < ApplicationController
+module Api
+  module V1
+    class ConcertsController < ApplicationController
+      def upcomingConcerts
+        concerts = Concert.where(location_id: current_user.location_id, upcoming: true)
+
+        render json: ConcertSerializer.new(concerts).serialized_json
+      end
+    end
+  end
 end
