5c031e9e49523ffc125c76c4bd44a1a2372a0cdc
Rachel <williamsrachelada@gmail.com>
2019-12-10 13:15:14 -0800

    Changed namespacing of controller to use modules, rendered json using user serializer to not return unwanted data

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,32 +1,36 @@
-class Api::V1::SessionsController < ApplicationController
-  def create
-    @user = User.find_by(username: params[:session][:username])
+module Api
+  module V1
+    class SessionsController < ApplicationController
+      def create
+        @user = User.find_by(username: params[:session][:username])
 
-    if @user && @user.authenticate(params[:session][:password])
-      session[:user_id] = @user.id
-      render json: @user
-    else
-      render json: {
-        error: "Invalid Credentials"
-      }
+        if @user && @user.authenticate(params[:session][:password])
+          session[:user_id] = @user.id
+          render json: UserSerializer.new(@user)
+        else
+          render json: {
+            error: "Invalid Credentials"
+          }
+        end
+      end
+
+      def getCurrentUser
+        if logged_in?
+          render json: UserSerializer.new(current_user)
+        else
+          render json: {
+            error: "You are not logged in"
+          }
+        end
+      end
+
+      def destroy
+        session.clear
+        render json: {
+          notice: "You are now logged out"
+        }, status: :ok
+      end
+
     end
   end
-
-  def getCurrentUser
-    if logged_in?
-      render json: current_user
-    else
-      render json: {
-        error: "You are not logged in"
-      }
-    end
-  end
-
-  def destroy
-    session.clear
-    render json: {
-      notice: "You are now logged out"
-    }, status: :ok
-  end
-
-end
+end 
