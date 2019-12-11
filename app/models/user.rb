50ded820632c82359577ddd6e8c3ee3147b508f9
Rachel <williamsrachelada@gmail.com>
2019-12-11 15:31:09 -0800

    Added validation for presence of user name, username, and password and unique username

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   has_many :saved_concerts
   has_many :concerts, through: :saved_concerts
   belongs_to :location
+
+  validates :name, :username, presence: true
+  validates :username, uniqueness: true
 end
