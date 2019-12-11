83136e1346971324583a1ca632fe18e1954a5782
Rachel <williamsrachelada@gmail.com>
2019-12-11 15:33:42 -0800

    Added user create action to add user to database. Added strong params for user sign up data

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -6,4 +6,26 @@ class Api::V1::UsersController < ApplicationController
     render json: users_json
   end
 
+  def create
+    @user = User.new(user_params)
+    location = Location.find_or_create_by(city: params[:user][:location][:city], state: params[:user][:location][:state], country: params[:user][:location][:country])
+    @user.location = location
+    
+    if @user.save
+      session[:user_id] = @user.id
+      render json: UserSerializer.new(@user).serialized_json, status: :created
+    else
+      response = {
+        error: @user.errors.full_messages.to_sentence
+      }
+      render json: response, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :username, :password)
+  end
+
 end
