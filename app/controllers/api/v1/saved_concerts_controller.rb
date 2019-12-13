8ca20a1eaf2345c1f648ccb714a23548b9a6fca0
Rachel <williamsrachelada@gmail.com>
2019-12-12 22:25:12 -0800

    Created saved concerts controller and added create action

---
+++ app/controllers/api/v1/saved_concerts_controller.rb
@@ -0,0 +1,26 @@
+module Api
+  module V1
+    class SavedConcertsController < ApplicationController
+
+      def create
+        # byebug
+        saved_concert = SavedConcert.new(saved_concert_params)
+
+        if saved_concert.save
+          render json: saved_concert, status: :created
+        else
+          response = {
+            error: saved_concert.errors.full_messages.to_sentence
+          }
+          render json: response, status: :unprocessable_entity
+        end
+      end
+
+      private
+
+      def saved_concert_params
+        params.require(:saved_concert).permit(:concert_id, :user_id)
+      end
+    end
+  end
+end
---
+++ test/controllers/saved_concerts_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class SavedConcertsControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
