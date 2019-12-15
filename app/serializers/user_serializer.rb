2d833f8082464ed0d5641541056d985555053b38
Rachel <williamsrachelada@gmail.com>
2019-12-15 15:22:36 -0800

    Added location_json attribute to populate users city, state, and country

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -2,4 +2,12 @@ class UserSerializer
   include FastJsonapi::ObjectSerializer
   attributes :name, :username
   belongs_to :location
+
+  attribute :location_json do |user|
+    {
+      city: user.location.city,
+      state: user.location.state,
+      country: user.location.country
+    }
+  end
 end
