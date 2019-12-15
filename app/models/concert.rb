4d3a7395635f4a017026a7f317c29195db80c6cc
Rachel <williamsrachelada@gmail.com>
2019-12-14 18:28:33 -0800

    Added dependent destroy for saved_concert records

--- app/models/concert.rb
+++ app/models/concert.rb
@@ -1,5 +1,5 @@
 class Concert < ApplicationRecord
-  has_many :saved_concerts
+  has_many :saved_concerts, dependent: :destroy
   has_many :users, through: :saved_concerts
   belongs_to :location
   belongs_to :artist
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   has_secure_password
-  has_many :saved_concerts
+  has_many :saved_concerts, dependent: :destroy
   has_many :concerts, through: :saved_concerts
   belongs_to :location
 
