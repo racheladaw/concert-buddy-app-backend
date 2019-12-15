82b11c44ed96365155fd64302e8401d96f225969
Rachel <williamsrachelada@gmail.com>
2019-12-14 18:29:10 -0800

    Added method to get concert data from seat geek using rest client

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,6 +1,13 @@
+require 'rest-client'
+
 class Location < ApplicationRecord
   has_many :concerts
   has_many :users
 
   validates :city, :state, :country, presence: true
+
+  def get_concert_data
+    seat_geek_url = "https://api.seatgeek.com/2/events?venue.city=#{self.city}&taxonomies.name=concert"
+    data = JSON.parse(RestClient.get("#{seat_geek_url}&client_id=#{ENV['SEAT_GEEK_CLIENT_ID']}"))
+  end
 end
