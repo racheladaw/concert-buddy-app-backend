a41f44f8f9688c9266896d227e96d7bdb7c65344
Rachel <williamsrachelada@gmail.com>
2019-12-16 12:35:13 -0800

    Added index action to get user's saved concert data

--- app/controllers/api/v1/saved_concerts_controller.rb
+++ app/controllers/api/v1/saved_concerts_controller.rb
@@ -7,13 +7,21 @@ module Api
         saved_concert = SavedConcert.new(saved_concert_params)
 
         if saved_concert.save
-          render json: saved_concert, status: :created
+          concert = Concert.find(saved_concert.concert_id)
+          render json: ConcertSerializer.new(concert).serialized_json, status: :created
         else
           response = {
             error: saved_concert.errors.full_messages.to_sentence
           }
           render json: response, status: :unprocessable_entity
         end
+      end
+
+      def index
+        user = User.find(current_user.id)
+        concerts = user.concerts
+
+        render json: ConcertSerializer.new(concerts).serialized_json
       end
 
       private
