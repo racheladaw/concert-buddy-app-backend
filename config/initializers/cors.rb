e646486f4a425f76e57c4699974c0df5f3839e42
Rachel <williamsrachelada@gmail.com>
2020-02-26 17:39:39 -0800

    Removed localhost from cors origins

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:3000', 'https://angry-goldberg-327bdc.netlify.com/'
+    origins 'https://angry-goldberg-327bdc.netlify.com'
 
     resource '*',
       headers: :any,
