9369edfc7388b9f5a86c3aa887fae873c61b2b4a
Rachel <williamsrachelada@gmail.com>
2020-03-08 11:36:31 -0700

    Added second allow block to allow requests from localhost during app development

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -15,4 +15,13 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
       # credentials added to include them in fetch requests
       credentials: true
   end
+
+  allow do
+    origins 'http://localhost:3000'
+
+    resource '*',
+    headers: :any,
+    methods: [:get, :post, :put, :patch, :delete, :options, :head],
+    credentials: true
+  end
 end
