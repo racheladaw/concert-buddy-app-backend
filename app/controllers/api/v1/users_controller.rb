1d14eee984afb74ad0c3fdc4094889f0946b087a
Rachel <williamsrachelada@gmail.com>
2020-04-01 11:05:47 -0700

    Added strong params for profile picture upload and added to uploadPhoto action to find a user and attach the profile picture file from the strong params.

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -10,7 +10,7 @@ class Api::V1::UsersController < ApplicationController
     @user = User.new(user_params)
     location = Location.find_or_create_by(city: params[:user][:location][:city], state: params[:user][:location][:state], country: params[:user][:location][:country])
     @user.location = location
-    
+
     if @user.save
       session[:user_id] = @user.id
       render json: UserSerializer.new(@user).serialized_json, status: :created
@@ -22,10 +22,22 @@ class Api::V1::UsersController < ApplicationController
     end
   end
 
+  def uploadPhoto
+    @user = User.find(profile_picture_params[:id])
+    # need functionality to take file from params and add photo to database
+
+    @user.profile_picture.attach(profile_picture_params[:profile_picture])
+    byebug
+  end
+
   private
 
   def user_params
     params.require(:user).permit(:name, :username, :password)
+  end
+
+  def profile_picture_params
+    params.permit(:profile_picture, :id)
   end
 
 end
