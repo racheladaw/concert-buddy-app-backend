7b52c6c2af088a1410ca3131e23ec7240e14c088
Rachel <williamsrachelada@gmail.com>
2020-05-04 16:24:22 -0700

    Edited netlify origin to be .app instead of .com.

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://angry-goldberg-327bdc.netlify.com'
+    origins 'https://angry-goldberg-327bdc.netlify.app'
 
     resource '*',
       headers: :any,
