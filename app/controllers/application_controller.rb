036460534d985e3b7c650d6669509f2f220181e2
Rachel <williamsrachelada@gmail.com>
2020-05-05 14:48:36 -0700

    Added root route to application

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,10 @@
 class ApplicationController < ActionController::API
   include ::ActionController::Cookies
 
+  def index
+    Hello World
+  end 
+
   def current_user
     User.find_by(id: session[:user_id])
   end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   # namespace routes to look like localhost:3000.com/api/v1/resource
+  root "application#index"
+
   namespace :api do
     namespace :v1 do
       post '/login', to: 'sessions#create'
