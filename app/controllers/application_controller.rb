1a2fab7b6f6470a4d9f014aff9f39a174fd6052d
Rachel <williamsrachelada@gmail.com>
2020-10-02 17:39:38 -0700

    Removed logs for debugging and a comment

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,8 +6,6 @@ class ApplicationController < ActionController::API
   end
 
   def current_user
-    puts "finding current user"
-    puts session[:user_id]
     User.find_by(id: session[:user_id])
   end
 
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -55,6 +55,4 @@ Rails.application.configure do
   Rails.application.routes.default_url_options = { host: "http://localhost:3001" }
 
   config.log_level = :debug
-
-  # config.session_store :cookie_store
 end
